2d4c84cd9e1ec6a1f1473521bc4543affd5a677b
gnitnaw <wanting@gmail.com>
2015-03-14 12:38:29 +0800

    fibonacci number with recursion

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -14,19 +14,34 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	SUBS r4, r0, #0
+
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	BLE .L3
 
 	@ Compare R4 wtih 1
+	CMP r4, #1
+
 	@ If R4 == 1 goto .L4 (which returns 1)
+	BEQ .L4
 
 	@ R0 = R4 - 1
+	SUB r0, r4, #1
+
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+	BL fibonacci
 
 	@ R5 = R0
+	MOV r5, r0
+
 	@ R0 = R4 - 2
+	SUB r0, r4, #2
+
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+	BL fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+	ADDS r0, r5, r0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
