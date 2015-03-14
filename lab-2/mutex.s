79276ef51d33b7bd9eb342a6022fc960dd188df8
gnitnaw <wanting@gmail.com>
2015-03-14 16:03:31 +0800

    Mutex exclusion

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -10,6 +10,29 @@
 lock_mutex:
         @ INSERT CODE BELOW
 
+	@ DEFINE r1 = 1
+	MOV r1, #1
+.L0:
+	@ Load and monitor the value at the memory pointed by r6 to r2
+	LDREX r2, [r6]
+
+	@ Compare the value of r2 and 1
+	CMP r2, #1
+
+	@ If r2 != 1
+	@ Then lock r2 and put the value = 1
+	STREXNE r2, r1, [r6]
+
+	@ Check if r2 is lock
+	CMP r2, #1
+
+	@ If r2 is still not 1
+	@ Retry
+	BNE .L0
+
+	@ Required before accessing protected resource
+	DMB
+
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +42,21 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+	@ Ensure accesses to protected resource have completed
+        DMB
+
+	@ Define r1 = 0
+	MOV r1, #0
+
+	@ Send the value@r1 to the memory pointed by r6
+	STR r1, [r6]
+
+	@ Ensure update of the mutex occurs before other CPUs wake
+	DSB
+
+	@ Send event to other cpu
+	SEV
+ 
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
